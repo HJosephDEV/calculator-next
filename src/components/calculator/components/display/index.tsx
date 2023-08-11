898b4d9c8ac3c49330526f41fb81faf7e1e71da9
Joseph Haase <joseph.haase.dev@gmail.com>
2023-08-10 21:37:55 -0300

    Changes the styles of the display

--- src/components/calculator/components/display/index.tsx
+++ src/components/calculator/components/display/index.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import { CalculatorDisplayProps } from '../../@types';
 import styles from './styles.module.scss';
 
@@ -5,18 +6,39 @@ export default function CalculatorDisplay({
   displayView,
   historic
 }: CalculatorDisplayProps): JSX.Element {
+  const historicWrapperRef = useRef<HTMLDivElement>(null);
+  const historicContainerRef = useRef<HTMLDivElement>(null);
+  const historicComponent: JSX.Element[] = historic.map((expression: string, i: number) => (
+    <div
+      key={`expression-${i}`}
+      className={styles.historicItem}
+    >
+      <div className={styles.itemContent}>{expression}</div>
+    </div>
+  ));
+
+  historicWrapperRef.current &&
+    historicWrapperRef.current.scrollTo(0, historicContainerRef.current?.offsetHeight || 0);
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        {historic.map((expression: string, i: number) => (
-          <div key={`expression-${i}`}>
-            {expression}
-            <br />
+        <div
+          className={styles.historicWrapper}
+          ref={historicWrapperRef}
+        >
+          <div
+            className={styles.historicContainer}
+            ref={historicContainerRef}
+          >
+            {historicComponent}
           </div>
-        ))}
-        {displayView.expression}
-        <br />
-        {!!displayView.result && `= ${displayView.result}`}
+        </div>
+
+        <div className={styles.visorContainer}>
+          {displayView.expression}
+          {!!displayView.result && `= ${displayView.result}`}
+        </div>
       </div>
     </div>
   );
